2ff45686bcdf200bb4be90ac3b7cd4f7bc63528b
lucianohorvath <lucianohorvath94@gmail.com>
2018-10-25 14:15:22 -0300

    Se modifica nuevamente Listado de Documentos.

--- SIGAB/SIGAB/ListadoDocumentos.Designer.cs
+++ SIGAB/SIGAB/ListadoDocumentos.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(746, 500);
+            this.ClientSize = new System.Drawing.Size(710, 488);
             this.Name = "ListadoDocumentos";
             this.Text = "Listado de Documentos";
             this.ResumeLayout(false);
