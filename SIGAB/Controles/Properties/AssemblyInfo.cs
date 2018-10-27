589ea112944d3bb2173fa8ebdb5ed4500e46e78b
Flori, Juan Manuel <JuanManuel.Flori@Alumnos.uai.edu.ar>
2018-10-27 20:14:37 -0300

    Revert "Eliminación del form creado por motivos de prueba."
    This reverts commit 9462c0f3290e184620fe39da263366a7f771caf7.

--- SIGAB/Controles/Properties/AssemblyInfo.cs
+++ SIGAB/Controles/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a7164faa-8847-4fda-8d57-d45516ae676d")]
+[assembly: Guid("a6661848-9c2c-4c3e-8099-b823f312b83b")]
 
 // Version information for an assembly consists of the following four values:
 //
--- SIGAB/SIGAB/Form2.cs
+++ SIGAB/SIGAB/Form2.cs
@@ -1,18 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Drawing;
 using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Controles
+namespace SIGAB
 {
-    public partial class TextBoxUAI : UserControl
+    public partial class Form2 : Form
     {
-        public TextBoxUAI()
+        public Form2()
         {
             InitializeComponent();
         }
