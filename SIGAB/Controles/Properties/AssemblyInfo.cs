00a2634d0ef8c71d716e5191413cccd06936c891
Flori, Juan Manuel <JuanManuel.Flori@Alumnos.uai.edu.ar>
2018-10-27 21:14:15 -0300

    Se creo el proyecto Controles que tiene todos los Controles custom nombrados con el patron ("nombreDelControl" + UAI ) y creados la mayoria de los controles.

--- SIGAB/Controles/Properties/AssemblyInfo.cs
+++ SIGAB/Controles/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a6661848-9c2c-4c3e-8099-b823f312b83b")]
+[assembly: Guid("381b9951-8b36-49da-b1bd-4adbf8755291")]
 
 // Version information for an assembly consists of the following four values:
 //
--- SIGAB/SIGAB/Form1.Designer.cs
+++ SIGAB/SIGAB/Form1.Designer.cs
@@ -34,9 +34,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(465, 275);
+            this.button1.Location = new System.Drawing.Point(310, 179);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(308, 118);
+            this.button1.Size = new System.Drawing.Size(205, 77);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -44,20 +45,22 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(465, 85);
+            this.button2.Location = new System.Drawing.Point(341, 71);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(308, 62);
+            this.button2.Size = new System.Drawing.Size(205, 40);
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1098, 617);
+            this.ClientSize = new System.Drawing.Size(732, 401);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
