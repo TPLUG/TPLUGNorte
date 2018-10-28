e6d965513f412a65a741cd2fa42cc1c188523425
Flori, Juan Manuel <JuanManuel.Flori@Alumnos.uai.edu.ar>
2018-10-28 17:19:35 -0300

    Creacion de los form asignados "Autoridades" y "Busqueda por Signatura Topografica"

--- SIGAB/SIGAB/Form1.Designer.cs
+++ SIGAB/SIGAB/Form1.Designer.cs
@@ -34,8 +34,8 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(310, 179);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(341, 132);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(205, 77);
             this.button1.TabIndex = 0;
@@ -46,12 +46,13 @@
             // button2
             // 
             this.button2.Location = new System.Drawing.Point(341, 71);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(205, 40);
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
@@ -60,7 +61,7 @@
             this.ClientSize = new System.Drawing.Size(732, 401);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
