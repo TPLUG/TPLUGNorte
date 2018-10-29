08bbc17f5bf1abd44d14f07e8c4bfe77bb34ba27
DESKTOP-SEF1AD1\Julian <julianeloy.detitto@gmail.com>
2018-10-30 00:32:42 +0100

    Agrego capas y form de personas. Jdetitto

---
+++ SIGAB/SIGAB/Personas.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace SIGAB
+{
+    partial class Personas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonUAI1 = new Controles.ButtonUAI();
+            this.buttonUAI2 = new Controles.ButtonUAI();
+            this.buttonUAI3 = new Controles.ButtonUAI();
+            this.buttonUAI4 = new Controles.ButtonUAI();
+            this.buttonUAI5 = new Controles.ButtonUAI();
+            this.buttonUAI6 = new Controles.ButtonUAI();
+            this.buttonUAI7 = new Controles.ButtonUAI();
+            this.dataGridViewUAI1 = new Controles.DataGridViewUAI();
+            this.labelUAI1 = new Controles.LabelUAI();
+            this.textBoxUAI1 = new Controles.TextBoxUAI();
+            this.Biblio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUAI1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonUAI1
+            // 
+            this.buttonUAI1.Location = new System.Drawing.Point(12, 12);
+            this.buttonUAI1.Name = "buttonUAI1";
+            this.buttonUAI1.Size = new System.Drawing.Size(104, 34);
+            this.buttonUAI1.TabIndex = 0;
+            this.buttonUAI1.Text = "Nuevo";
+            this.buttonUAI1.UseVisualStyleBackColor = true;
+            // 
+            // buttonUAI2
+            // 
+            this.buttonUAI2.Location = new System.Drawing.Point(122, 12);
+            this.buttonUAI2.Name = "buttonUAI2";
+            this.buttonUAI2.Size = new System.Drawing.Size(104, 34);
+            this.buttonUAI2.TabIndex = 1;
+            this.buttonUAI2.Text = "Editar";
+            this.buttonUAI2.UseVisualStyleBackColor = true;
+            // 
+            // buttonUAI3
+            // 
+            this.buttonUAI3.Location = new System.Drawing.Point(232, 12);
+            this.buttonUAI3.Name = "buttonUAI3";
+            this.buttonUAI3.Size = new System.Drawing.Size(104, 34);
+            this.buttonUAI3.TabIndex = 2;
+            this.buttonUAI3.Text = "Ver";
+            this.buttonUAI3.UseVisualStyleBackColor = true;
+            // 
+            // buttonUAI4
+            // 
+            this.buttonUAI4.Location = new System.Drawing.Point(342, 12);
+            this.buttonUAI4.Name = "buttonUAI4";
+            this.buttonUAI4.Size = new System.Drawing.Size(104, 34);
+            this.buttonUAI4.TabIndex = 3;
+            this.buttonUAI4.Text = "Media";
+            this.buttonUAI4.UseVisualStyleBackColor = true;
+            // 
+            // buttonUAI5
+            // 
+            this.buttonUAI5.Location = new System.Drawing.Point(452, 12);
+            this.buttonUAI5.Name = "buttonUAI5";
+            this.buttonUAI5.Size = new System.Drawing.Size(104, 34);
+            this.buttonUAI5.TabIndex = 4;
+            this.buttonUAI5.Text = "Borrar";
+            this.buttonUAI5.UseVisualStyleBackColor = true;
+            // 
+            // buttonUAI6
+            // 
+            this.buttonUAI6.Location = new System.Drawing.Point(562, 12);
+            this.buttonUAI6.Name = "buttonUAI6";
+            this.buttonUAI6.Size = new System.Drawing.Size(104, 34);
+            this.buttonUAI6.TabIndex = 5;
+            this.buttonUAI6.Text = "Imprimir";
+            this.buttonUAI6.UseVisualStyleBackColor = true;
+            // 
+            // buttonUAI7
+            // 
+            this.buttonUAI7.Location = new System.Drawing.Point(672, 12);
+            this.buttonUAI7.Name = "buttonUAI7";
+            this.buttonUAI7.Size = new System.Drawing.Size(104, 34);
+            this.buttonUAI7.TabIndex = 6;
+            this.buttonUAI7.Text = "Salir";
+            this.buttonUAI7.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewUAI1
+            // 
+            this.dataGridViewUAI1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewUAI1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Biblio,
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7});
+            this.dataGridViewUAI1.Location = new System.Drawing.Point(12, 61);
+            this.dataGridViewUAI1.Name = "dataGridViewUAI1";
+            this.dataGridViewUAI1.RowTemplate.Height = 28;
+            this.dataGridViewUAI1.Size = new System.Drawing.Size(764, 294);
+            this.dataGridViewUAI1.TabIndex = 7;
+            // 
+            // labelUAI1
+            // 
+            this.labelUAI1.AutoSize = true;
+            this.labelUAI1.Location = new System.Drawing.Point(12, 362);
+            this.labelUAI1.Name = "labelUAI1";
+            this.labelUAI1.Size = new System.Drawing.Size(82, 20);
+            this.labelUAI1.TabIndex = 8;
+            this.labelUAI1.Text = "Búsqueda";
+            // 
+            // textBoxUAI1
+            // 
+            this.textBoxUAI1.Location = new System.Drawing.Point(100, 362);
+            this.textBoxUAI1.Name = "textBoxUAI1";
+            this.textBoxUAI1.Size = new System.Drawing.Size(676, 26);
+            this.textBoxUAI1.TabIndex = 9;
+            // 
+            // Biblio
+            // 
+            this.Biblio.HeaderText = "Biblio";
+            this.Biblio.Name = "Biblio";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "T.Persona";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Codigo";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Suj";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Apellido / Razon";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Nombre";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Sector";
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Telefono";
+            this.Column7.Name = "Column7";
+            // 
+            // Personas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 400);
+            this.Controls.Add(this.textBoxUAI1);
+            this.Controls.Add(this.labelUAI1);
+            this.Controls.Add(this.dataGridViewUAI1);
+            this.Controls.Add(this.buttonUAI7);
+            this.Controls.Add(this.buttonUAI6);
+            this.Controls.Add(this.buttonUAI5);
+            this.Controls.Add(this.buttonUAI4);
+            this.Controls.Add(this.buttonUAI3);
+            this.Controls.Add(this.buttonUAI2);
+            this.Controls.Add(this.buttonUAI1);
+            this.Name = "Personas";
+            this.Text = "Personas";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUAI1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Controles.ButtonUAI buttonUAI1;
+        private Controles.ButtonUAI buttonUAI2;
+        private Controles.ButtonUAI buttonUAI3;
+        private Controles.ButtonUAI buttonUAI4;
+        private Controles.ButtonUAI buttonUAI5;
+        private Controles.ButtonUAI buttonUAI6;
+        private Controles.ButtonUAI buttonUAI7;
+        private Controles.DataGridViewUAI dataGridViewUAI1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Biblio;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private Controles.LabelUAI labelUAI1;
+        private Controles.TextBoxUAI textBoxUAI1;
+    }
+}
