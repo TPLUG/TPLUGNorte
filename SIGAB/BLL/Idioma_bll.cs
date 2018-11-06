c1d4b452c9d104112fb64b848a040ff4d7e65ff8
lucianohorvath <lucianohorvath94@gmail.com>
2018-11-06 16:19:34 -0300

    Se agregan las clases Pais e Idioma en las capas BLL, MAPPER y ENTIDADES. En la clase AccesoSQLServer (DAL) se agrega un método para ejecutar un SP y devolver un DataReader, y otro método para ejecutar un SP con parámetros y devolver un int.

---
+++ SIGAB/BLL/Idioma_bll.cs
@@ -0,0 +1,28 @@
+﻿using ENTIDADES;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class Idioma_bll
+    {
+        public int Agregar(Idioma_en i)
+        {
+
+            return 0;
+        }
+
+        public int Borrar(Idioma_en i)
+        {
+            return 0;
+        }
+
+        public Idioma_en Traer(int id)
+        {
+            return new Idioma_en();
+        }
+    }
+}
