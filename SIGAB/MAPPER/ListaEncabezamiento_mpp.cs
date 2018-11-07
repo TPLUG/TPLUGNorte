3a7af86e756e8b9469289e018708276b3d593b25
juan.flori <juan.flori@JFLORI-PC>
2018-11-07 16:10:14 -0300

    Metodos ListaEncabezamiento

--- SIGAB/MAPPER/ListaEncabezamiento_mpp.cs
+++ SIGAB/MAPPER/ListaEncabezamiento_mpp.cs
@@ -5,12 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data;
 using DAL;
+using System.Data.SqlClient;
+using ENTIDADES;
 
 namespace MAPPER
 {
     public class ListaEncabezamiento_mpp
     {
-        public void Guardar(ENTIDADES.ListaEncabezamiento_en encabezadoEntidad)
+        public int Agregar(ListaEncabezamiento_en encabezadoEntidad)
         {
             AccesoSQLServer sql = new AccesoSQLServer();
             List<object[]> parametros = new List<object[]>();
@@ -18,22 +20,44 @@ namespace MAPPER
             object[] param2 = { "@detalle", encabezadoEntidad.detalle };
             parametros.Add(param1);
             parametros.Add(param2);
-            //return sql.EjecutarQuery_DS("Rol_Agregar", parametros);
+            return sql.EjecutarSP_int("Lista_Encabezamiento_Agregar", parametros);
         }
-        public void Borrar(ENTIDADES.ListaEncabezamiento_en encabezadoEntidad)
+        public int Borrar(int id)
         {
+            AccesoSQLServer sql = new AccesoSQLServer();
+            List<object[]> parametros = new List<object[]>();
+            object[] param1 = { "@cod_encabezamiento", id };           
+            parametros.Add(param1);
+            return sql.EjecutarSP_int("Lista_Encabezamiento_Agregar", parametros);
+        }
 
-        }
-
-        public DataSet Traer(ENTIDADES.ListaEncabezamiento_en encabezadoEntidad)
+        public ListaEncabezamiento_en Traer(int id)
         {
-            DataSet ds = new DataSet();
-                return ds;
+            ListaEncabezamiento_en encabezado = null;
+            AccesoSQLServer sql = new AccesoSQLServer();
+            SqlDataReader dr = sql.EjecutarSP_DR("Operador_Traer"/*,id*/);
+            if (dr.Read())
+            {
+                encabezado = new ListaEncabezamiento_en();
+                encabezado.codEncabezamiento = Convert.ToInt32(dr["cod_encabezamiento"]);
+                encabezado.detalle = dr["detalle"].ToString();
+            }
+            return encabezado;
         }
-        public DataSet TraerTodo()
+        public List<ListaEncabezamiento_en> TraerTodo()
         {
-            DataSet ds = new DataSet();
-            return ds;
+            List<ListaEncabezamiento_en> listaEncabezados = new List<ListaEncabezamiento_en>();
+            ListaEncabezamiento_en encabezado = null;
+            AccesoSQLServer sql = new AccesoSQLServer();
+            SqlDataReader dr = sql.EjecutarSP_DR("Lista_Encabezamiento_TraerTodos");
+            if (dr.Read())
+            {
+                encabezado = new ListaEncabezamiento_en();
+                encabezado.codEncabezamiento = Convert.ToInt32(dr["cod_encabezamiento"]);
+                encabezado.detalle = dr["detalle"].ToString();
+                listaEncabezados.Add(encabezado);
+            }
+            return listaEncabezados;
         }
     }
 }
